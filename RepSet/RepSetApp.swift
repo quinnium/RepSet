ceaa1d696ca184d8c79844d6e2aaf29b788248aa
quinnium <78171763+quinnium@users.noreply.github.com>
2024-08-23 12:12:00 +0100

    Initial Commit

---
+++ RepSet/RepSetApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepSetApp.swift
+//  RepSet
+//
+//  Created by Quinn on 23/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct RepSetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
